054a62b29c531eb995a33108920c19cb71b43cea
LiYiHeng <13331593+DesignerCoderY@user.noreply.gitee.com>
2024-03-29 15:45:52 +0800

    chore:unocss配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
+import { URL, fileURLToPath } from 'node:url'
 
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,10 +8,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCSS(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
